d2ac4301d788940e7e61823e2b62215b6420ae46
adex-hub <adeolaweb@gmail.com>
2023-08-12 16:10:39 +0100

    .

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css'
 import Qrcode from './components/qrcode';
 function App() {
   return (
-    <section className="App">
+    <main className="App">
       <Qrcode src='/images/image-qr-code.png' alt="qr"/>
       <header>Improve your front-end skills by building projects</header>
       <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
-    </section>
+    </main>
   );
 }
 
