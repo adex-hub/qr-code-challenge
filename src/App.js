ce96ecf46def35f4ac2d3feb646fd18db6ea7bae
adex-hub <adeolaweb@gmail.com>
2023-08-12 16:08:12 +0100

    Done ✅

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <section className="App">
       <Qrcode src='/images/image-qr-code.png' alt="qr"/>
-      <h1>Improve your front-end skills by building projects</h1>
+      <header>Improve your front-end skills by building projects</header>
       <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </section>
   );
